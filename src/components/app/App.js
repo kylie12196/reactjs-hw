ef5c0a6404c43b2637a190eec62e493baa9c2a2b
nguyetpm-1863 <pham.thi.minh.nguyet@sun-asterisk.com>
2020-09-29 08:19:16 +0700

    Hw-01-basic-dom

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../scss/main.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header title">Header</div>
+      <div className="app__main title">Body</div>
+    </div>
+  );
+}
+
+export default App;
