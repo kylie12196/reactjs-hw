cc80ff090b300162172cdd7dfab99b7a3532b12d
kylie12196 <phamminhnguyet96@gmail.com>
2020-11-02 22:53:40 +0700

    Update app.js

--- src/modules/layout/app.js
+++ src/modules/layout/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../scss/main.scss';
-import Header from '../layout/header';
+import Header from '../layout/header';
 import Main from '../layout/main';
 
 function App() {
